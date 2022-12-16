789760ec033e526543d84d12faa500db9a534037
francifle <franciflee@gmail.com>
2022-12-16 10:03:45 -0300

    Se cambio LoginController y se agregaron Parcials

--- src/main/java/com/crisalis/controllers/LoginController.java
+++ src/main/java/com/crisalis/controllers/LoginController.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.crisalis.models.Usuario;
 import com.crisalis.repositories.UsuarioRepository;
